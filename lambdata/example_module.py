a9b2fc12fd93e4a5e8ae774e3e7cb62441ea0bc6
Micah <micahswain@gmail.com>
2020-10-29 15:45:14 -0700

    Created and ran Unit Test

--- lambdata/example_module.py
+++ lambdata/example_module.py
@@ -11,6 +11,7 @@ FAVORITE_NUMBERS = [2, 3, 45, 53, 2.3]
 class MyDataFrame(pd.DataFrame):
     def state_abb(self, col):
         """adds a column to a DF with state abbreivations"""
+        
         us_state_abbrev = {'ALABAMA': 'AL',
                             'ALASKA': 'AK',
                             'AMERICAN SAMOA': 'AS',
@@ -68,10 +69,18 @@ class MyDataFrame(pd.DataFrame):
                             'WISCONSIN': 'WI',
                             'WYOMING': 'WY'
                             }
-        
+
+        # for state in self[col]:
+        #     if state.upper() in us_state_abbrev.keys:
+
         self['state_abbreviation'] = self[col].str.upper().map(us_state_abbrev)
-        
+                
         return self
+            
+            # else:
+
+            #     print('DF columns contains a non-state')
+
         
 def outlier_rm(self, df, col):
     IQR = np.quantile(df[col], 0.75) - np.quantile(df[col], 0.25)
--- lambdata/lambda_test.py
+++ lambdata/lambda_test.py
@@ -1,59 +1,32 @@
 """Basic unit test for lambdata package"""
 
 import unittest
+import pandas as pd
+import numpy as np
 
-from example_module import COLORS, increment
+from example_module import MyDataFrame
 
-
-
-class SocialMediaUserTests(unittest.TestCase):
-    """Tests for usage of Social Media Users in our Social Media"""
-    def setup(self):
-        self.user1 = SocialMediaUser(name="Jimmy", location="France")
-        self.user2 = SocialMediaUser(name="Craig", location="Kenya")
-
-    def test_name(self):
-        self.assertEqual(self.user1.name, "Jimmy")
-        self.assertEqual(self.user2.name, "Craig")
-
-    # def test_location(self):
-    #     """Testing Location attribute"""
-    #     self.assertEqual(self.user1.location, "France")
-    #     self.assertEqual(self.user2.location, "Kenya")
-
-class ExampleTests(unittest.TestCase):
-  """Making sure examples work as expected"""
-  def test_increment(self):
-    """Testing increment adds one to a number"""
-    x0 = 0
-    y0 = increment(x0) #y0 == 1
-    self.assertEqual(y0, 1)
-
-    x1 = 100
-    y1 = increment(x1) # y1 == 101
-    self.assertEqual(y1, 101)
-
-    x2 = -1
-    y2 = increment(x2) # y2 == 0
-    self.assertEqual(y2, 0)
-
-    x3 = -1.5
-    y3 = increment(x3) # x3 == -0.5
-    self.assertEqual(y3, -0.5)
-
-  def test_colors(self):
-    """Testing the presence/absence of members in the colors list"""
-    self.assertIn("cyan", COLORS)
-    self.assertNotIn("brown", COLORS)
-
-  def test_number_colors(self):
-    """Testing that we have the expect number of colors"""  
-    self.assertEqual(len(COLORS), 3)
+class MyDataFrameTests(unittest.TestCase):
+  """Tests for usage of MyDataFrame class"""
+  def setUp(self):
+    self.df1 = MyDataFrame({'State': ['Hawaii', 'California', 'Texas', 'new york']})
+    self.df2 = MyDataFrame({'LOCATION': ['NEVADA', 'WYOMING', 'JAPAN', 'FLORIDA']})
+    self.df3 = MyDataFrame({'place': ['american samoa', 'guam', 'idaho', 'utah']})
     
 
-
+    self.df1a = self.df1.state_abb('State')
+    self.df1b = self.df2.state_abb('LOCATION')
+    self.df1c = self.df3.state_abb('place')
+    
+  
+  def test_abb_column(self):
+    """Testing the state_abb method"""
+    self.assertEqual(self.df1a['state_abbreviation'].to_list(), ['HI', 'CA', 'TX', 'NY'])
+    self.assertEqual(self.df1b['state_abbreviation'].to_list(), ['NV', 'WY', np.nan, 'FL'])
+    self.assertEqual(self.df1c['state_abbreviation'].to_list(), ['AS', 'GU', 'ID', 'UT'])
+    
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
 
 
